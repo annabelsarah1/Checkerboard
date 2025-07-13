9fd452fdcf95bbdc8e225e575054bad3a1dc2512
Annabel Andrews <aannabelsarah@gmail.com>
2025-07-13 09:34:06 -0700

    Add files via upload
    Initial commit of Java Checkerboard Project

---
+++ Checkerboard.java
@@ -0,0 +1,13 @@
+public class Checkerboard
+{
+	private int size;
+	//size is an integer variable only privately viewed accessible only in this class
+	public CheckerBoard(int x)
+	{
+		this.size = x; //Checkerboard of size SIZE is constructed
+}
+	public int getSize()
+	{
+		return this.size;
+}
+}
